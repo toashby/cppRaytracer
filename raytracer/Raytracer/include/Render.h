5cc5216f969e44871e88af9f6a5d1a4550cda47e
Tom <tashby@live.co.uk>
2019-01-22 12:27:34 +0000

    added all files

---
+++ raytracer/Raytracer/include/Render.h
@@ -0,0 +1,15 @@
+#ifndef RENDER_H
+#define RENDER_H
+#include "Scene.h"
+
+class Render
+{
+public:
+    Render();
+    Render(Scene s);
+
+    int resx;
+    int resy;
+};
+
+#endif // RENDER_H
---
+++ raytracer/Raytracer/src/Render.cpp
@@ -0,0 +1,6 @@
+#include "Render.h"
+#include <iostream>
+#include <fstream>
+#include <eigen3/Eigen/Geometry>
+#include "Ray.h"
+#include "Sphere.h"
